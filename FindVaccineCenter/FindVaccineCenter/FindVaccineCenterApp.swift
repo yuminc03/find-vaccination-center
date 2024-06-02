4052561daf08ff962b74d02592c17987fd40b1d9
Yumin Chu <yuminc03@gmail.com>
2024-06-02 22:35:13 +0900

    Feat: 프로젝트 생성

---
+++ FindVaccineCenter/FindVaccineCenter/FindVaccineCenterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindVaccineCenterApp.swift
+//  FindVaccineCenter
+//
+//  Created by Yumin Chu on 6/2/24.
+//
+
+import SwiftUI
+
+@main
+struct FindVaccineCenterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
