60236a31481569cd786e5f8b6b55625c4dad8b50
Yumin Chu <yuminc03@gmail.com>
2024-07-11 23:09:34 +0900

    Chore: LocationEntity -> CenterDetailEntity 네이밍 수정

---
+++ FindVaccineCenter/Scene/Detail/Entity/CenterDetailEntity.swift
@@ -0,0 +1,29 @@
+import Foundation
+import MapKit
+
+/// 예방접종 센터 상세 화면 데이터
+struct CenterDetailEntity: Equatable, Identifiable {
+  /// 예방 접종 센터명
+  let name: String
+  /// 위치
+  let coordinate: CLLocationCoordinate2D
+  /// 주소
+  let address: String
+  /// 시설명
+  let facilityName: String
+  
+  var id: String {
+    return name + address
+  }
+  
+  static func == (lhs: CenterDetailEntity, rhs: CenterDetailEntity) -> Bool {
+    return lhs.id == rhs.id
+  }
+  
+  static let dummy = CenterDetailEntity(
+    name: "코로나19 중앙 예방접종센터",
+    coordinate: .init(latitude: 37.567817, longitude: 127.004501),
+    address: "서울특별시 중구 을지로 39길 29",
+    facilityName: "국립중앙의료원 D동"
+  )
+}
