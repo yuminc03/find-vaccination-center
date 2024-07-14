6f035e913df2f0479f6d35dfbdc15f5161591794
Yumin Chu <yuminc03@gmail.com>
2024-07-14 23:16:36 +0900

    Refactor: 지도에 센터가 Marker로 표시되도록 수정, TCA 적용

--- FindVaccineCenter/App/AppView.swift
+++ FindVaccineCenter/App/AppView.swift
@@ -6,9 +6,26 @@ import ComposableArchitecture
 @Reducer
 struct AppCore {
   struct State: Equatable {
+    static func == (lhs: AppCore.State, rhs: AppCore.State) -> Bool {
+      return lhs.vaccinations == rhs.vaccinations &&
+      lhs.entity == rhs.entity &&
+      lhs.error == rhs.error &&
+      lhs.locationError == rhs.locationError &&
+      lhs.searchText == rhs.searchText &&
+      lhs.currentRegion.center.latitude == rhs.currentRegion.center.latitude &&
+      lhs.currentRegion.center.longitude == rhs.currentRegion.center.longitude
+    }
+    
     var vaccinations: VaccinationCenterEntity?
-    var searchResults: [VaccinationCenterEntity.Vaccnination]?
+    var entity: [CenterDetailEntity] = []
     var error: VCError?
+    var currentRegion = MKCoordinateRegion(
+      center: .init(
+        latitude: 37.4802547,
+        longitude: 126.9742529
+      ),
+      span: .init(latitudeDelta: 0.01, longitudeDelta: 0.01)
+    )
     
     @BindingState var locationError: VCError.LocationError?
     @BindingState var searchText = ""
@@ -19,6 +36,7 @@ struct AppCore {
   enum Action: BindableAction, Equatable {
     case binding(BindingAction<State>)
     case didTapSearchButton
+    case changeLocation(Double, Double)
     
     case requestVaccination
     case _vaccinationResponse(Result<VaccinationCenterEntity, VCError>)
@@ -35,6 +53,15 @@ struct AppCore {
           await send(.requestVaccination)
         }
         
+      case let .changeLocation(lat, lng):
+        state.currentRegion = MKCoordinateRegion(
+          center: .init(
+            latitude: lat,
+            longitude: lng
+          ),
+          span: .init(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        )
+        
       case .requestVaccination:
         state.error = nil
         return .run { send in
@@ -45,9 +72,22 @@ struct AppCore {
         }
         
       case let ._vaccinationResponse(.success(dto)):
-        let searchedData = dto.data.filter { $0.centerName.contains(state.searchText) }
+        let searchedData = dto.data.filter { $0.centerName.contains(state.searchText)
+        }
         state.vaccinations = dto
-        state.searchResults = searchedData
+        
+        state.entity = []
+        searchedData.forEach {
+          state.entity.append(
+            .init(
+              name: $0.centerName,
+              coordinate: CLLocationCoordinate2D(
+                latitude: Double($0.lat) ?? 0,
+                longitude: Double($0.lng) ?? 0),
+              address: $0.address,
+              facilityName: $0.facilityName)
+          )
+        }
         
       case let ._vaccinationResponse(.failure(error)):
         state.error = error
@@ -61,21 +101,18 @@ struct AppCore {
 struct AppView: View {
   private let store: StoreOf<AppCore>
   @ObservedObject private var viewStore: ViewStoreOf<AppCore>
+  
   @StateObject private var locationService = LocationAuthorityService.shared
-  @State private var currentRegion = MKCoordinateRegion(
-    center: .init(
-      latitude: 37.35959299,
-      longitude: 127.10531600
-    ),
-    span: .init(latitudeDelta: 0.01, longitudeDelta: 0.01)
-  )
   
   private var region: Binding<MKCoordinateRegion> {
     return .init(
-      get: { currentRegion },
+      get: { viewStore.currentRegion },
       set: { newValue in
         DispatchQueue.main.async {
-          currentRegion = newValue
+          store.send(.changeLocation(
+            newValue.center.latitude,
+            newValue.center.longitude
+          ))
         }
       }
     )
@@ -88,9 +125,9 @@ struct AppView: View {
   
   var body: some View {
     ZStack {
-      Map(coordinateRegion: region, showsUserLocation: true)
-        .ignoresSafeArea()
-      searchView
+      MapView
+      
+      SearchView
     }
     .onAppear {
       locationService.initialize()
@@ -99,48 +136,52 @@ struct AppView: View {
       guard let latitude = $0?.coordinate.latitude,
             let longitude = $0?.coordinate.longitude
       else { return }
-      currentRegion = .init(
-        center: .init(latitude: latitude, longitude: longitude),
-        span: .init(latitudeDelta: 0.01, longitudeDelta: 0.01)
-      )
+      store.send(.changeLocation(latitude, longitude))
     }
     .environmentObject(locationService)
   }
 }
 
 private extension AppView {
-  var searchView: some View {
-    VStack {
+  var MapView: some View {
+    Map(
+      coordinateRegion: region,
+      showsUserLocation: true,
+      annotationItems: viewStore.entity
+    ) { location in
+      MapAnnotation(coordinate: location.coordinate) {
+        MapAnnotationView()
+      }
+    }
+    .ignoresSafeArea()
+  }
+  
+  var SearchView: some View {
+    VStack(spacing: 0) {
       HStack(spacing: 10) {
-        searchBar
-        searchButton
+        TextField("주소를 입력해주세요", text: viewStore.$searchText)
+          .padding(.horizontal, 20)
+          .padding(.vertical, 15)
+          .background(.white)
+          .clipShape(RoundedRectangle(cornerRadius: 10))
+          .shadow(radius: 10)
+        
+        Button {
+          store.send(.didTapSearchButton)
+        } label: {
+          Image(systemName: .systemImage(.magnifyingglass))
+            .resizable()
+            .frame(width: 20, height: 20)
+            .foregroundColor(.white)
+            .padding(15)
+            .background(Color.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .shadow(radius: 10)
+        }
       }
       .padding(.horizontal, 16)
+      
       Spacer()
-    }
-  }
-  
-  var searchBar: some View {
-    TextField("주소를 입력해주세요", text: viewStore.$searchText)
-      .padding(.horizontal, 20)
-      .padding(.vertical, 15)
-      .background(.white)
-      .clipShape(RoundedRectangle(cornerRadius: 10))
-      .shadow(radius: 10)
-  }
-  
-  var searchButton: some View {
-    Button {
-      store.send(.didTapSearchButton)
-    } label: {
-      Image(systemName: .systemImage(.magnifyingglass))
-        .resizable()
-        .frame(width: 20, height: 20)
-        .foregroundColor(.white)
-        .padding(15)
-        .background(Color.blue)
-        .clipShape(RoundedRectangle(cornerRadius: 10))
-        .shadow(radius: 10)
     }
   }
 }
