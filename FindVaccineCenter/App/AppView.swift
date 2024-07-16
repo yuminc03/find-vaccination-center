4a96517d5e59fba176031be47790d0760fd89270
Yumin Chu <yuminc03@gmail.com>
2024-07-16 23:07:18 +0900

    Fix: 지도 움직이지 않는 문제, Marker 표시되지 않는 문제 수정

--- FindVaccineCenter/App/AppView.swift
+++ FindVaccineCenter/App/AppView.swift
@@ -6,26 +6,9 @@ import ComposableArchitecture
 @Reducer
 struct AppCore {
   struct State: Equatable {
-    static func == (lhs: AppCore.State, rhs: AppCore.State) -> Bool {
-      return lhs.vaccinations == rhs.vaccinations &&
-      lhs.entity == rhs.entity &&
-      lhs.error == rhs.error &&
-      lhs.locationError == rhs.locationError &&
-      lhs.searchText == rhs.searchText &&
-      lhs.currentRegion.center.latitude == rhs.currentRegion.center.latitude &&
-      lhs.currentRegion.center.longitude == rhs.currentRegion.center.longitude
-    }
-    
     var vaccinations: VaccinationCenterEntity?
     var entity: [CenterDetailEntity] = []
     var error: VCError?
-    var currentRegion = MKCoordinateRegion(
-      center: .init(
-        latitude: 37.4802547,
-        longitude: 126.9742529
-      ),
-      span: .init(latitudeDelta: 0.01, longitudeDelta: 0.01)
-    )
     
     @BindingState var locationError: VCError.LocationError?
     @BindingState var searchText = ""
@@ -53,19 +36,12 @@ struct AppCore {
           await send(.requestVaccination)
         }
         
-      case let .changeLocation(lat, lng):
-        state.currentRegion = MKCoordinateRegion(
-          center: .init(
-            latitude: lat,
-            longitude: lng
-          ),
-          span: .init(latitudeDelta: 0.01, longitudeDelta: 0.01)
-        )
+      case let .changeLocation(lat, lng): break
         
       case .requestVaccination:
         state.error = nil
         return .run { send in
-          let dto = try await repo.requestVaccinationCenter()
+          let dto = try await repo.requestVaccinationCenter(dataCount: 30)
           await send(._vaccinationResponse(.success(dto)))
         } catch: { error, send in
           await send(._vaccinationResponse(.failure(error.toVCError)))
@@ -77,16 +53,32 @@ struct AppCore {
         state.vaccinations = dto
         
         state.entity = []
-        searchedData.forEach {
-          state.entity.append(
-            .init(
+        if searchedData.count == 0 {
+          dto.data.forEach {
+            state.entity.append(.init(
               name: $0.centerName,
               coordinate: CLLocationCoordinate2D(
                 latitude: Double($0.lat) ?? 0,
-                longitude: Double($0.lng) ?? 0),
+                longitude: Double($0.lng) ?? 0
+              ),
               address: $0.address,
-              facilityName: $0.facilityName)
-          )
+              facilityName: $0.facilityName
+            ))
+          }
+        } else {
+          searchedData.forEach {
+            state.entity.append(
+              .init(
+                name: $0.centerName,
+                coordinate: CLLocationCoordinate2D(
+                  latitude: Double($0.lat) ?? 0,
+                  longitude: Double($0.lng) ?? 0
+                ),
+                address: $0.address,
+                facilityName: $0.facilityName
+              )
+            )
+          }
         }
         
       case let ._vaccinationResponse(.failure(error)):
@@ -104,16 +96,19 @@ struct AppView: View {
   
   @StateObject private var locationService = LocationAuthorityService.shared
   
+  @State private var currentRegion = MKCoordinateRegion(
+    center: .init(
+      latitude: 37.4802547,
+      longitude: 126.9742529
+    ),
+    span: .init(latitudeDelta: 0.1, longitudeDelta: 0.1)
+  )
+  
   private var region: Binding<MKCoordinateRegion> {
-    return .init(
-      get: { viewStore.currentRegion },
+    .init(
+      get: { currentRegion },
       set: { newValue in
-        DispatchQueue.main.async {
-          store.send(.changeLocation(
-            newValue.center.latitude,
-            newValue.center.longitude
-          ))
-        }
+        DispatchQueue.main.async { currentRegion = newValue }
       }
     )
   }
@@ -131,12 +126,17 @@ struct AppView: View {
     }
     .onAppear {
       locationService.initialize()
+      store.send(.requestVaccination)
     }
     .onReceive(locationService.$currentLocation) {
       guard let latitude = $0?.coordinate.latitude,
             let longitude = $0?.coordinate.longitude
       else { return }
-      store.send(.changeLocation(latitude, longitude))
+      
+      currentRegion = .init(
+        center: .init(latitude: latitude, longitude: longitude),
+        span: .init(latitudeDelta: 0.1, longitudeDelta: 0.1)
+      )
     }
     .environmentObject(locationService)
   }
@@ -146,7 +146,6 @@ private extension AppView {
   var MapView: some View {
     Map(
       coordinateRegion: region,
-      showsUserLocation: true,
       annotationItems: viewStore.entity
     ) { location in
       MapAnnotation(coordinate: location.coordinate) {
@@ -165,6 +164,7 @@ private extension AppView {
           .background(.white)
           .clipShape(RoundedRectangle(cornerRadius: 10))
           .shadow(radius: 10)
+          .foregroundColor(.black)
         
         Button {
           store.send(.didTapSearchButton)
