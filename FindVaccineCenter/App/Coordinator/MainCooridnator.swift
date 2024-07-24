1d230740111a8f3e9bb765d1a2b7e0574222287f
Yumin Chu <yuminc03@gmail.com>
2024-07-24 22:49:24 +0900

    Feat: MainCoordinator 추가

--- FindVaccineCenter/App/Coordinator/MainCooridnator.swift
+++ FindVaccineCenter/App/Coordinator/MainCooridnator.swift
@@ -3,11 +3,60 @@ import SwiftUI
 import TCACoordinators
 import ComposableArchitecture
 
-//@Reducer
-//struct MainCooridnator {
-//  struct State: Equatable {
-//    static let initialState = State(routes: )
-//    var routes: IdentifiedArrayOf<Route<>>
-//  }
-//}
+@Reducer
+struct MainCoordinator {
+  struct State: Equatable {
+    static let initialState = State(routes: [.root(.map(.init()), embedInNavigationView: true)])
+    var routes: IdentifiedArrayOf<Route<MainScreen.State>>
+  }
+  
+  enum Action {
+    case router(IdentifiedRouterActionOf<MainScreen>)
+  }
+  
+  var body: some ReducerOf<Self> {
+    Reduce { state, action in
+      switch action {
+      default: break
+      }
+      
+      return .none
+    }
+    .forEachRoute(\.routes, action: \.router) {
+      MainScreen()
+    }
+  }
+}
 
+struct MainCoordinatorView: View {
+  private let store: StoreOf<MainCoordinator>
+  
+  var body: some View {
+    TCARouter(store.scope(state: \.routes, action: \.router)) { screen in
+      SwitchStore(screen) { screen in
+        switch screen {
+        case .map:
+          CaseLet(
+            \MainScreen.State.map,
+             action: MainScreen.Action.map,
+             then: AppView.init
+          )
+          
+        case .search:
+          CaseLet(
+            \MainScreen.State.search,
+             action: MainScreen.Action.search,
+             then: SearchView.init
+          )
+          
+        case .detail:
+          CaseLet(
+            \MainScreen.State.detail,
+             action: MainScreen.Action.detail,
+             then: DetailView.init
+          )
+        }
+      }
+    }
+  }
+}
